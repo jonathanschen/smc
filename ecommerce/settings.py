d33a36a7558f97b9fc5543f0aa3fcc34446f87c8
Jonathan Chen <jonathanschen@gmail.com>
2012-08-18 22:10:44 -0400

    my django app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,7 +14,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost'),
+
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
